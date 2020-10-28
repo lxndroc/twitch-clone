388710339d2e0db28f872a33faa173970395852d
Alexandros Xafopoulos <42879364+lxndroc@users.noreply.github.com>
2020-10-28 23:23:21 +0000

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 // https://github.com/lxndroc/twitch-clone
+// References:
+//  1. youtube.com/watch?v=0pFqFGwL9mA
+//  2. youtube.com/watch?v=f9wxmJx0oXE
+//  3. youtube.com/watch?v=ygVEEkBsNWE
 // npx create-react-app twitch-clone
-// font-awesome copy CDN to index.html
-// npm i @material-ui/core
 
 import React from 'react'
 import Header from './Header'
